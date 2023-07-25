e455550d10e12fdcfc5588fe7da00a33059b31e3
uc-sag <sagar.gupta@ucertify.com>
2023-07-25 10:27:58 +0530

    PHP Function Parameters - Named Arguments

--- index.php
+++ index.php
@@ -1,28 +1,39 @@
 <?php
-// How to Create functions in PHP
-foo();
-bar();     // It will work only it it is called after foo() call
-function foo(){
-    echo 'foo','<br/>';
-    function bar(){
-        echo 'bar','<br/>';
-    }
-}
-if(true){
-    function xyx(){
-        echo 'xyz','<br/>';
-    }
-}
-
-xyz();    //it cna be called only after condition expression in run and it is true
-
-
-function sum(int $x,int $y): int{
+// PHP function parameters
+declare(strict_types=1);
+function foo(int|float $x ,int|float $y)
+{
     return $x + $y;
 }
 
-var_dump(sum(2,5));
+echo foo(5.0,10),'<br/>';
 
+function sum(...$numbers):int|float
+{
+    $tot = 0;
+    foreach($numbers as $val)
+    {
+        $tot += $val;
+    }
+    return $tot;
+}
+
+echo sum(2,4,5,7.5),'<br/>';
+
+
+function bar(int $x,int $y){
+    var_dump($x);
+    if($x % $y ==0){
+        return "Yes";
+    }
+    else{
+        return "No";
+    }
+}
+
+$x = 5;
+$y =3;
+echo bar(y:$y,x:$x);  // Named Parameter
 
 
 ?>
