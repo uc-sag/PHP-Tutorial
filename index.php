eee2ceb06cd902ad9a7782bbd3b6c27f232a5c34
uc-sag <sagar.gupta@ucertify.com>
2023-07-25 09:57:10 +0530

    How to Create functions in PHP

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?php
-// How to include files in php
-require 'file.php';          //include will return warning if file is not found but require return error and stop excution of script
+// How to Create functions in PHP
+foo();
+bar();     // It will work only it it is called after foo() call
+function foo(){
+    echo 'foo','<br/>';
+    function bar(){
+        echo 'bar','<br/>';
+    }
+}
+if(true){
+    function xyx(){
+        echo 'xyz','<br/>';
+    }
+}
 
-require_once 'file.php';      //it include only one time
+xyz();    //it cna be called only after condition expression in run and it is true
 
 
-$x = include 'file2.php';
-var_dump($x);
+function sum(int $x,int $y): int{
+    return $x + $y;
+}
+
+var_dump(sum(2,5));
+
+
+
 ?>
