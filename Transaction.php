86dca0b4fe8e2d502181f38f924a74359ec77ad3
uc-sag <sagar.gupta@ucertify.com>
2023-07-25 23:46:26 +0530

    Static Properties & Methods

--- Transaction.php
+++ Transaction.php
@@ -6,9 +6,12 @@ class Transaction
    public const STATUS_PENDING = 'pending';
    public const STATUS_DECLINED = 'declined';
 
+   public static int $count =0;
+
    public function __construct()
    {
      var_dump(self::STATUS_PAID);
+     self::$count++;
    }
 
     
--- index.php
+++ index.php
@@ -6,4 +6,6 @@ echo Transaction::STATUS_PAID;
 
 $transaction = new Transaction();
 
+// echo $transaction->count;     it cannot be acces because it is static 
+var_dump(Transaction::$count);
 ?>
