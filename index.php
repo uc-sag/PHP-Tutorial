0e5f7aff716e560faa729e0225b2ab83e7734029
uc-sag <sagar.gupta@ucertify.com>
2023-07-25 08:34:41 +0530

    PHP Operator Precedance & Associativity

--- index.php
+++ index.php
@@ -1,49 +1,21 @@
 <?php
-//OPERATORS
-#Error control Operators
-$x = @file('foo.txt');
+// PHP Operator Precedance & Associativity
+$x = 5+ 3* 2;
 
-#Increment and Decrement Operator
-$x = 5;
-echo ++$x ,'<br/>';
-echo --$x ,'<br/>';
+echo $x ,'<br/>';
 
-$x = null;
-echo --$x ,'<br/>';
-echo ++$x ,'<br/>';
+$x = 3;
+$y =7;
+$z = 2;
+$res = $x < $y;
+echo $res ,'<br/>';
 
-#Logical 
-$x = 1;
-$y = 0;
-var_dump($x || $y);
-var_dump($x or $y);
-var_dump($x and $y);
-var_dump($x && $y);
-
-#Short Circuiting
-function Hii(){
-    echo "Hii";
-    return true;
-}
-
-var_dump($x || Hii());
+$x = true;
+$y =false;
+$z = true;
+var_dump($x && $y || $z);
 
 
-// Bitwise Operatorf 
-$x = 6;
-$y =3;
-var_dump($x & $y);
-
-// Array Operators
-$x = ['a','b','c','d'];
-$y = ['e','f','g','h','i','j'];
-$z = $x +$y;
-print_r($z);
-
-$x = ['a'=>1,'b'=>2,'c'=>3,'d'=>4];
-$y = ['e'=>1,'f'=>3,'g'=>3];
-$z = $x +$y;
-print_r($z);
 
 
 ?>
