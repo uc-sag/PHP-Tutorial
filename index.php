4b7579f121bfe9cde8e9625c9b4a6c73ca827369
uc-sag <sagar.gupta@ucertify.com>
2023-07-25 17:01:59 +0530

    PHP Classes & Objects - Typed Properties - Constructors & Destructors

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
+require_once 'Transaction.php';
+$transaction = new Transaction(100,'Transaction');
+// $transaction->amount = 15;
+$transaction->addTax(8);
+$transaction->applyDiscount(10);
 
-// Mini Exercise Project Overview
+var_dump($transaction->amount);
+
+unset($transaction);      // same as  $transaction = null;   
+                          // if we exit the also destruct is called
+
+
+$obj = new \stdClass;
+$obj->a = 1;
+$obj->b = 5;
+
+var_dump($obj);
 
 ?>
