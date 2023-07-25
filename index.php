80b4539e0d33d9b5c44cbbd9c3e5c516f5ff9923
uc-sag <sagar.gupta@ucertify.com>
2023-07-25 13:00:52 +0530

    Basic Apache Webserver Configuration & Virtual Hosts

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-// PHP Error Handling & Error Handlers
-error_reporting(0);    //it will not show errors
-error_reporting(E_ALL);    //it will show all errors including warning
-error_reporting(E_ALL & ~E_WARNING);    //it will show all errors except warning
+// Basic Apache Webserver Configuration & Virtual Hosts
 
 ?>
