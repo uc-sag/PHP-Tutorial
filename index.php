6f72897a688de4b3813be220e64aca1dcdf9c369
uc-sag <sagar.gupta@ucertify.com>
2023-07-25 11:37:38 +0530

    Date and Time Zone

--- index.php
+++ index.php
@@ -1,61 +1,13 @@
 <?php
-//Variable anonymous and callable function
-function sum(int|float ...$numbers): int|float{
-    return array_sum($numbers);
-}
-
-echo sum(1,2,3,4).'<br/>';
-
-$x = 'sum';
-if(is_callable($x)){
-    echo $x(1,2,3,4);
-}
-else{
-    echo "Not callable";
-}
-
-//Anonymous function end with semicolon
-$x =1;
-$sum = function(int|float ...$numbers) use(&$x): int|float{
-    $x = 15;
-    echo $x;
-    return array_sum($numbers);
-};
-echo $sum(2,3,4);
-echo '<br/>',$x;
-
-//callable functions
-
-$array = [1,2,3,4];
-function foo($element){
-   return $element *2;
-}
-$array2 = array_map('foo',$array);
-echo '<pre>';
-print_r($array);
-
-print_r($array2);
-echo '</pre>';
+//Date and Time Zones
 
 
-$sum = function(callable $callback, int|float ...$numbers): int|float{
-    return $callback(array_sum($numbers));
-};
-echo $sum(function($element){
-   return $element *2;
-},1,2,3,4);
+$currentTime = time();
+echo $currentTime .'<br/>';
+echo $currentTime + 5*24 *60*60 .'<br/>';
+echo date('m/d/y g:ia') .'<br/>';
+echo date('m/d/y g:ia', $currentTime + 5*24 *60*60) .'<br/>';
 
-
-//Arrow functions
-$array = [1,2,3,4];
-$x = 2;
-$array2 = array_map(fn($numb)=> $numb * $numb * $x ,$array);    // we can access the parent scope variable in arrow function without the use keyword     
-echo '<pre>';
-print_r($array);                                               //if we modify the varible of parent scope it is not modified for parent
-
-print_r($array2);
-echo '</pre>';
-
-
+date_default_timezone_set('UTC');
 
 ?>
