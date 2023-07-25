ab3a277600acb04ee92f445b41aebabbc64b6c3e
uc-sag <sagar.gupta@ucertify.com>
2023-07-25 11:24:04 +0530

    Variable, Anonymous, Callable, Closure & Arrow Functions In PHP

--- index.php
+++ index.php
@@ -1,21 +1,61 @@
 <?php
-//PHP variables scopes - Static Variables
-$x = 5;
-include 'script1.php';
-
-echo $x.'<br/>';
-function foo(){
-    // sleep(2);
- echo "foo-",$GLOBALS['x'],'<br/>';  
-}
-foo();
-
-function bar(){
-    static $val = 0;
-    echo "bar called ".++$val."times".'<br/>';
+//Variable anonymous and callable function
+function sum(int|float ...$numbers): int|float{
+    return array_sum($numbers);
 }
 
-bar();
-bar();
+echo sum(1,2,3,4).'<br/>';
+
+$x = 'sum';
+if(is_callable($x)){
+    echo $x(1,2,3,4);
+}
+else{
+    echo "Not callable";
+}
+
+//Anonymous function end with semicolon
+$x =1;
+$sum = function(int|float ...$numbers) use(&$x): int|float{
+    $x = 15;
+    echo $x;
+    return array_sum($numbers);
+};
+echo $sum(2,3,4);
+echo '<br/>',$x;
+
+//callable functions
+
+$array = [1,2,3,4];
+function foo($element){
+   return $element *2;
+}
+$array2 = array_map('foo',$array);
+echo '<pre>';
+print_r($array);
+
+print_r($array2);
+echo '</pre>';
+
+
+$sum = function(callable $callback, int|float ...$numbers): int|float{
+    return $callback(array_sum($numbers));
+};
+echo $sum(function($element){
+   return $element *2;
+},1,2,3,4);
+
+
+//Arrow functions
+$array = [1,2,3,4];
+$x = 2;
+$array2 = array_map(fn($numb)=> $numb * $numb * $x ,$array);    // we can access the parent scope variable in arrow function without the use keyword     
+echo '<pre>';
+print_r($array);                                               //if we modify the varible of parent scope it is not modified for parent
+
+print_r($array2);
+echo '</pre>';
+
+
 
 ?>
